bb68f5fe0e52d1b2151ffcbc8a1fc5942d7b9516
selvinjethu <60860622+selvinjethu@users.noreply.github.com>
2022-06-26 21:03:25 +0530

    addded
    assw

---
+++ test.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+variable "prefix" {
+  description = "servername prefix"
+  default     = "gritfyapp"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  count         = 10
+  vpc_security_group_ids = [
+    "sg-043fa81204ac9ced3"
+  ]
+  user_data = <<EOF
+#!/bin/bash
+sudo echo "ubuntu:data2go!" | chpasswd
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo /etc/init.d/ssh restart
+
+
+EOF
+
+}
+
+output "instances" {
+  value       = aws_instance.web.*.private_ip
+  description = "PrivateIP address details"
+}
