00f13e2a9ae90da21d6138710928caaa1f8dc298
selvinjethu <60860622+selvinjethu@users.noreply.github.com>
2022-06-27 21:50:26 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,8 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-  #with:
-    #    aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
-     #   aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+
 }
 
 variable "prefix" {
